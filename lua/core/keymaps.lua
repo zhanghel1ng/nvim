8a34e2aa0ab19ee419b954ddb6cd7c4a20c1f1f1
xiaoQuanTxue <zhang.heling@foxmail.com>
2022-10-03 00:41:10 +0800

    first commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,105 @@
+local function set_bg_light()
+	vim.cmd('set background=light')
+	local colors_name = vim.g.colors_name
+	vim.cmd('colorscheme shine')
+	vim.cmd('colorscheme ' .. colors_name)
+end
+
+local function set_bg_dark()
+	vim.cmd('set background=dark')
+	local colors_name = vim.g.colors_name
+	vim.cmd('colorscheme ron')
+	vim.cmd('colorscheme ' .. colors_name)
+end
+
+vim.g.mapleader = ';'
+
+-- keymaps
+vim.keymap.set('i', '<C-g>', '<esc>')
+vim.keymap.set('i', '<C-;>', '::') -- for C++ and Rust
+vim.keymap.set('n', '<leader>vl', set_bg_light)
+vim.keymap.set('n', '<leader>vd', set_bg_dark)
+vim.keymap.set('n', '<leader>', ':')
+-- f: file tree
+vim.keymap.set('n', '<F3>', ':NvimTreeToggle<cr>')
+vim.keymap.set('n', '<leader>ft', ':NvimTreeToggle<cr>')
+vim.keymap.set('n', '<leader>ff', ':NvimTreeFocus<cr>')
+-- y: telescope
+vim.keymap.set('n', '<F9>', function() require 'telescope.builtin'.find_files {} end)
+vim.keymap.set('n', '<F10>', function() require 'telescope.builtin'.git_files {} end)
+vim.keymap.set('n', '<F11>', function() require 'telescope.builtin'.buffers {} end)
+vim.keymap.set({ 'n', 'i' }, '<C-p>', function() require 'telescope.builtin'.registers {} end)
+-- w: window
+vim.keymap.set('n', '<leader>w1', '<c-w>o')
+vim.keymap.set('n', '<leader>wx', ':x<cr>')
+vim.keymap.set('n', '<leader>w2', ':sp<cr>')
+vim.keymap.set('n', '<leader>w3', ':vs<cr>')
+-- window resize
+vim.keymap.set('n', '<m-9>', '<c-w><')
+vim.keymap.set('n', '<m-0>', '<c-w>>')
+vim.keymap.set('n', '<m-->', '<c-w>-')
+vim.keymap.set('n', '<m-=>', '<c-w>+')
+-- b: buffer
+vim.keymap.set('n', '<leader>bn', ':bn<cr>')
+vim.keymap.set('n', '<leader>bp', ':bp<cr>')
+vim.keymap.set('n', '<leader>bd', ':Bdelete<cr>')
+-- p: plugins
+vim.keymap.set('n', '<leader>pi', ':PackerInstall<cr>')
+vim.keymap.set('n', '<leader>pc', ':PackerClean<cr>')
+-- s: search
+vim.keymap.set('n', '<leader>ss', '/')
+vim.keymap.set('n', '<leader>sw', '/\\<lt>\\><left><left>')
+-- l/g/w: language
+-- l: general
+-- g: goto
+-- w: workspace
+-- c: inlay hints
+vim.keymap.set('n', '<leader>le', ':Lspsaga show_line_diagnostics<cr>')
+vim.keymap.set('n', '<leader>lE', ':Lspsaga show_cursor_diagnostics<cr>')
+vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>lk', vim.lsp.buf.hover)
+vim.keymap.set('n', '<leader>ld', ':Lspsaga preview_definition<cr>')
+vim.keymap.set('n', '<leader>lr', ':Lspsaga rename<cr>')
+vim.keymap.set('n', '<leader>lh', vim.lsp.buf.signature_help)
+vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.formatting)
+vim.keymap.set('n', '<leader>lb', ':SymbolsOutline<cr>')
+vim.keymap.set('n', '<leader>la', ':Lspsaga code_action<cr>')
+vim.keymap.set('n', '<leader>lu', ':Lspsaga lsp_finder<cr>')
+vim.keymap.set('n', '<F12>', ':Lspsaga code_action<cr>')
+vim.keymap.set('n', '<leader>it', function() require('rust-tools.inlay_hints').toggle_inlay_hints() end)
+vim.keymap.set('n', '<leader>is', function() require('rust-tools.inlay_hints').set_inlay_hints() end)
+vim.keymap.set('n', '<leader>id', function() require('rust-tools.inlay_hints').diable_inlay_hints() end)
+vim.keymap.set('n', '<f4>', ':SymbolsOutline<cr>')
+
+vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration)
+vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition)
+vim.keymap.set('n', '<leader>gt', vim.lsp.buf.type_definition)
+vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation)
+vim.keymap.set('n', '<leader>gp', ':Lspsaga diagnostic_jump_prev<cr>')
+vim.keymap.set('n', '<leader>gn', ':Lspsaga diagnostic_jump_next<cr>')
+vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references)
+
+vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder)
+vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder)
+vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end)
+
+-- t: terminal
+-- use <f5> to toggle terminal, this can be set in lua/configs/terminal.lua
+-- the default position is also set in lua/configs/terminal.lua
+vim.keymap.set('t', '<C-g>', '<C-\\><C-n>')
+vim.keymap.set('n', '<leader>tt', ':ToggleTerm direction=tab<cr>')
+vim.keymap.set('n', '<leader>tn', function() require('toggleterm.terminal').Terminal:new():toggle() end)
+vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<cr>')
+vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal<cr>')
+vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical<cr>')
+
+-- h: git
+vim.keymap.set('n', '<leader>hu', ':Gitsigns undo_stage_hunk<cr>')
+vim.keymap.set('n', '<leader>hn', ':Gitsigns next_hunk<cr>')
+vim.keymap.set('n', '<leader>hc', ':Gitsigns preview_hunk<cr>')
+vim.keymap.set('n', '<leader>hr', ':Gitsigns reset_hunk<cr>')
+vim.keymap.set('n', '<leader>hR', ':Gitsigns reset_buffer')
+vim.keymap.set('n', '<leader>hb', ':Gitsigns blame_line<cr>')
+vim.keymap.set('n', '<leader>hd', ':Gitsigns diffthis<cr>')
+vim.keymap.set('n', '<leader>hs', ':<C-U>Gitsigns select_hunk<CR>')
